b98feb25df0f4ee606a0f646d37629c27fe51711
Mike Wright <wrightmikea@gmail.com>
2017-09-07 18:09:26 -0700

    start adding cli support

--- src/leiningen/new/wma_clj.clj
+++ src/leiningen/new/wma_clj.clj
@@ -4,13 +4,29 @@
 
 (def render (renderer "wma-clj"))
 
-(defn wma-clj
+(defn clj-cli
+  "generates a Clojure CLI project"
+  [name]
+  (prn "tbd cli" name))
+
+(defn clj-lib
   "generates a Clojure library project"
   [name]
-  (let [data {:name name
-              :sanitized (name-to-path name)}]
-    (main/info "Generating fresh 'lein new' wma-clj 'library' project.")
-    (->files data
-             ["project.clj" (render "project.clj" data)]
-             ["src/{{sanitized}}/{{sanitized}}.clj" (render "foo.clj" data)]
-             ["test/{{sanitized}}/{{sanitized}}_test.clj" (render "foo_test.clj" data)])))
+     (let [data {:name name
+                 :sanitized (name-to-path name)}]
+       (main/info "Generating fresh 'lein new' wma-clj 'library' project.")
+       (->files data
+                ["project.clj" (render "project.clj" data)]
+                ["src/{{sanitized}}/{{sanitized}}.clj" (render "foo.clj" data)]
+                ["test/{{sanitized}}/{{sanitized}}_test.clj"
+                 (render "foo_test.clj" data)])))
+
+(defn wma-clj
+  "generates a leiningen project"
+  ([name]
+   (wma-clj name "lib"))
+  ([name type]
+   (condp = type
+     "cli" (clj-cli name)
+     "lib" (clj-lib name)
+     :else (prn "unrecognized type" type))))
