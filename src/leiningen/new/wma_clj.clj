86a21eef68f3d1e0dc8112a7aec0e2a115e90ce0
Mike Wright <wrightmikea@gmail.com>
2017-09-07 19:24:13 -0700

    refactoring

--- src/leiningen/new/wma_clj.clj
+++ src/leiningen/new/wma_clj.clj
@@ -12,21 +12,42 @@
 (defn clj-lib
   "generates a Clojure library project"
   [name]
-     (let [data {:name name
+  (prn "tbd lib" name)
+  (let [data {:name name
                  :sanitized (name-to-path name)}]
        (main/info "Generating fresh 'lein new' wma-clj 'library' project.")
        (->files data
-                ["project.clj" (render "project.clj" data)]
-                ["src/{{sanitized}}/{{sanitized}}.clj" (render "foo.clj" data)]
+                ["project.clj" (render "lib/project.clj" data)]
+                ["src/{{sanitized}}/{{sanitized}}.clj"
+                 (render "lib/foo.clj" data)]
                 ["test/{{sanitized}}/{{sanitized}}_test.clj"
-                 (render "foo_test.clj" data)])))
+                 (render "lib/foo_test.clj" data)])))
+
+(defn cljs-app
+  "generates a ClojureScript browser application"
+  [name]
+  (prn "tbd app" name))
+
+(defn cljs-cli
+  "generates a ClojureScript CLI project"
+  [name]
+  (prn "tbd node-cli" name))
+
+(defn cljs-lib
+  "generates a ClojureScript Libraru project"
+  [name]
+  (prn "tbd node-lib" name))
 
 (defn wma-clj
   "generates a leiningen project"
   ([name]
    (wma-clj name "lib"))
-  ([name type]
+  ([name type & opts]
+   (when opts (prn opts))
    (condp = type
+     "app" (cljs-app name)
      "cli" (clj-cli name)
      "lib" (clj-lib name)
-     :else (prn "unrecognized type" type))))
+     "node-cli" (cljs-cli name)
+     "node-lib" (cljs-lib name)
+     (prn "unrecognized type" type))))
