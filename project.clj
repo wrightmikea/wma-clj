491a80bc3b320489dff4c9c9f6a34cba1fb3d17d
Mike Wright <wrightmikea@gmail.com>
2017-10-04 08:37:04 -0700

    release 0.2.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wma-clj/lein-template "0.2.3-SNAPSHOT"
+(defproject wma-clj/lein-template "0.2.3"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
